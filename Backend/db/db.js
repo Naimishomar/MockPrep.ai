bd5f3fd2bed9146fa0b3de38d46b2fa9ef6770b4
Naimish Omar <naimishomar@gmail.com>
2025-05-10 23:13:27 +0530

    Updated

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.connectDb = async()=>{
     try {
-       const connect = await mongoose.connect('mongodb://localhost:27017/MockPrep') 
+       const connect = await mongoose.connect(process.env.MONGODB_URL) 
        console.log("Database connected successfully");   
     } catch (error) {
         console.log(error);
